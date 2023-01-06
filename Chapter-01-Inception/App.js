967a81baa711107f7ba4c675d376b875129b2c04
Niharika Thakur <niharikat80@gmail.com>
2023-01-06 19:37:34 +0530

    codeing-assignment-chapter-1

---
+++ Chapter-01-Inception/App.js
@@ -0,0 +1,21 @@
+const heading1 = React.createElement("h1",
+{
+    id: "title1"
+},
+"Heading 1");
+
+const heading2 = React.createElement("h2",
+{
+    id: "title2"
+},
+"Heading 2")
+
+const container = React.createElement("div",
+{
+    id: "container"
+},
+[heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//passing react element into root
+root.render(container);
